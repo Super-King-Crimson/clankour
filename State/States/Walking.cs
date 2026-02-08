61f9344a6fea8566723e2213909a64d77bc7df91
Roland Dillon <roland.dillon7676@gmail.com>
2026-02-07 22:11:16 -0500

    add momentum to walking state

--- State/States/Walking.cs
+++ State/States/Walking.cs
@@ -7,13 +7,19 @@ public partial class Walking : MovementState3D
     [Export] public MovementState3D airborneState;
     [Export] public MovementState3D jumpState;
 
-    [Export] public float baseSpeed = 5f;
-    [Export] public float currentSpeed = 0f;
+    [Export] public double currentSpeed = 0.0;
+    [Export] public float acceleration = 20.0f;
+    [Export] public float friction = 20.0f;
+
+    [Export] public double minimumSlideDotProd = -0.2;
+    [Export] public float rotationSpeed = 0.1f;
 
     [Export] public Node3D character;
 
     public override MovementState3D ProcessPhysics(double delta)
     {
+        base.ProcessPhysics(delta);
+
         if (!_agent.IsOnFloor())
             return airborneState;
 
@@ -21,17 +27,37 @@ public partial class Walking : MovementState3D
             return jumpState;
 
         Vector2 inputDir = this.GetInputDirection();
-        if (inputDir == Vector2.Zero)
-            return idleState;
-
         Vector3 direction = _agent.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y);
 
-        // optionally model face towards direction player is moving
+        var prevVel = _agent.Velocity;
+
+        if (inputDir == Vector2.Zero)
+        {
+            if (!prevVel.IsZeroApprox())
+            {
+                var deltaV2 = friction * (float)delta;
+                _agent.Velocity = prevVel.Normalized() * Math.Max(0, prevVel.Length() - deltaV2);
+
+                GD.Print($"before it was {prevVel}, now {_agent.Velocity}");
+
+                if (_agent.Velocity != Vector3.Zero)
+                    return null;
+            }
+
+            return idleState;
+        }
+
+        // TODO: Change to slide state
+        if (!prevVel.IsZeroApprox() && prevVel.Normalized().Dot(direction) < minimumSlideDotProd) return idleState;
+
+        var deltaV = acceleration * (float)delta;
+        var goalVel = direction * (prevVel.Length() + deltaV);
+
+        _agent.Velocity = prevVel.Slerp(goalVel, rotationSpeed);
+
         if (character is not null)
-            character.LookAt(_agent.Position + direction);
+            character.LookAt(_agent.Position + _agent.Velocity);
 
-        _agent.Velocity = direction * baseSpeed;
-
-        return base.ProcessPhysics(delta);
+        return null;
     }
 }
