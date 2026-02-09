937034ed2922be4fd996cc0ed68c529c2ba9afa6
Roland Dillon <roland.dillon7676@gmail.com>
2026-02-09 00:36:29 -0500

    add coyote time

--- State/States/Airborne.cs
+++ State/States/Airborne.cs
@@ -5,14 +5,21 @@ public partial class Airborne : MovementState3D
 {
     [Export] public GroundedState3D walkState;
     [Export] public MovementState3D idleState;
+    [Export] public MovementState3D jumpState;
+    [Export] public float coyoteTime = 0.25f;
 
     private void BaseEnterMethod(StringName _) => base.Enter(null);
     private bool _connected = false;
+    private float _coyoteTimer = 0.0f;
 
     public override MovementState3D Enter(State prevState)
     {
+        _coyoteTimer = 0.0f;
+
         if (prevState is Jumping)
         {
+            _coyoteTimer = this.coyoteTime;
+
             _connected = true;
             _animator.AnimationFinished += BaseEnterMethod;
 
@@ -38,11 +45,19 @@ public partial class Airborne : MovementState3D
     public override MovementState3D ProcessPhysics(double delta)
     {
         base.ProcessPhysics(delta);
-        _agent.Velocity += _agent.GetGravity() * (float)delta;
+
+        float fdelta = (float)delta;
+
+        _coyoteTimer += fdelta;
+        _agent.Velocity += _agent.GetGravity() * fdelta;
 
         if (_agent.IsOnFloor())
-        {
             return this.GetInputDirection() == Vector2.Zero ? idleState : walkState;
+
+        if (_coyoteTimer < this.coyoteTime)
+        {
+            if (this.WantsJump())
+                return jumpState;
         }
 
         return null;
