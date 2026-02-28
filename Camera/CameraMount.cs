75e6559193d36260657fa884695887721cfef607
Roland Dillon <roland.dillon7676@gmail.com>
2026-02-28 04:06:19 -0500

    add shift to toggle cameralock

--- Camera/CameraMount.cs
+++ Camera/CameraMount.cs
@@ -1,5 +1,3 @@
-#nullable enable
-
 using Godot;
 using System;
 
@@ -11,16 +9,43 @@ public partial class CameraMount : Node3D
     [Export] public bool camInvertH = true;
     [Export] public bool camInvertV = true;
 
-    [Export] public CharacterBody3D? agent;
+    [Export] public CharacterBody3D Agent { get; set; } = null!;
+
+    private bool _capturingMouse = false;
+    [Export]
+    public bool CapturingMouse
+    {
+        get => _capturingMouse;
+        set
+        {
+            if (_capturingMouse != value)
+            {
+                _capturingMouse = value;
+                ToggleMouseCapture();
+            }
+        }
+    }
+
+    public void ToggleMouseCapture()
+    {
+        if (_capturingMouse)
+        {
+            Input.MouseMode = Input.MouseModeEnum.Captured;
+        }
+        else
+        {
+            Input.MouseMode = Input.MouseModeEnum.Visible;
+        }
+    }
 
     public override void _Input(InputEvent e)
     {
-        if (e is InputEventMouseMotion motionEvent)
+        if (e is InputEventMouseMotion motionEvent && CapturingMouse)
         {
             int invH = camInvertH ? -1 : 1;
             int invV = camInvertV ? -1 : 1;
 
-            agent?.RotateY(invH * camSensH * Mathf.DegToRad(motionEvent.Relative.X));
+            Agent.RotateY(invH * camSensH * Mathf.DegToRad(motionEvent.Relative.X));
             this.RotateX(invV * camSensV * Mathf.DegToRad(motionEvent.Relative.Y));
 
             this.RotationDegrees = new Vector3(
@@ -29,10 +54,9 @@ public partial class CameraMount : Node3D
                 this.RotationDegrees.Z
             );
         }
-    }
-
-    public override void _Ready()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Captured;
+        else if (e.IsActionPressed("camlock") && !e.IsEcho())
+        {
+            CapturingMouse = !CapturingMouse;
+        }
     }
 }
