66ebfa8f4c9c47cc0374c7004abad2ae99ff4f3d
Roland Dillon <roland.dillon7676@gmail.com>
2026-02-28 03:25:35 -0500

    update rotate

--- State/StateMachineNode.cs
+++ State/StateMachineNode.cs
@@ -73,18 +73,47 @@ public abstract partial class StateMachineNode : Node
 
     protected Vector3 rotate(Vector3 fromVec, Vector3 toVec, float rad)
     {
-        float degBetween = fromVec.AngleTo(toVec);
+        float radBetween = fromVec.AngleTo(toVec);
 
         Vector3 goalRot = toVec;
 
-        if (degBetween > rad)
+        if (radBetween > rad)
         {
-            // HACK: if degBetween is exactly 180 (radBetween = pi), game doesn't know where to rotate
-            // to fix, rotate by small small fraction of a rad on agent's y transform
-            // so it can correctly guess the direction in which to rotate
-            if (degBetween - Mathf.Pi <= Mathf.Epsilon) fromVec = fromVec.Rotated(getAgent().Transform.Basis.Y, Mathf.Epsilon);
+            // if degBetween is nearly 180 (radBetween = pi), game doesn't know where to rotate
+            if (Mathf.IsEqualApprox(radBetween, Mathf.Pi))
+            {
+                // always rotate consistently, use character vs agent transform as tie breaker
+                // we always want to nudge inwards wrt camera, and camera is locked to agent
+                // what is inwards defined mathematically?
+                // we can draw a bird's eye view of player to make problem 2D (we're rotating based on TransformY)
+                // then we can draw rotation arrows based on the direction we want to rotate a bunch of arbitrary vectors
+                // see explain.png, and see how everything converges on the line of y = -x 
+                // going back to 3D, we can always get TransformX and Z, which map to local right and back respectively
+                // so if we just combine these two vectors and normalize them, we get the direction we want to rotate
+                // BUT WHAT IF WE'RE FACING THAT DIRECTION? (OR THE DIRECT OPPOSITE DIRECTION???)
+                // Calm down. Consult the graph again.
+                // Imagine what you would expect to happen if you were directly on that line.
+                // You'd rotate towards y = x (local right and forward) right? Ok cool. Now code the nudge.
+                // Man, if only we had a function that allowed us to rotate some angle between two vectors...
+                var agentBasis = getAgent().Transform.Basis;
+                var axis = agentBasis.Y;
+                var goalVec = (agentBasis.X + agentBasis.Z).Normalized();
 
-            goalRot = fromVec.Slerp(toVec, rad / degBetween);
+                // if facing goal vector
+                if (Mathf.IsEqualApprox(Mathf.Abs(fromVec.Normalized().Dot(goalVec)), 1))
+                {
+                    goalVec.Z = -1 * goalVec.Z;
+                }
+
+                // oh wait. that's THIS function!
+                // we're only multiplying by 2 here to COMPLETELY ENSURE we're out of the range and won't have a recursive call
+                // We change the goalVec if absolute of the dot was EqualApprox to 1 (which uses Epsilon),
+                // so using this we can ensure we bypass that check
+                fromVec = rotate(fromVec, goalVec, 2 * Mathf.Epsilon);
+            }
+
+            // now code the whole move, which will be encoded properly!
+            goalRot = fromVec.Slerp(toVec, rad / radBetween);
         }
 
         return goalRot;
